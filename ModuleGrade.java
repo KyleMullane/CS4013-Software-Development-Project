1c47c982d28966482134135de4f7d5ba29ae005b
KyleMullane <152344059+KyleMullane@users.noreply.github.com>
2023-12-04 18:42:36 +0000

    Update ModuleGrade.java

--- ModuleGrade.java
+++ ModuleGrade.java
@@ -8,6 +8,13 @@ public class ModuleGrade {
     private double grade;
     private double credits;
 
+    /**
+     * Method Creats an Object "ModuleGrade"
+     * @param moduleName Module's Name
+     * @param moduleCode Module's Unique Code
+     * @param grade Grade
+     * @param credits Module's Available Credits
+     */
     public ModuleGrade(String moduleName, String moduleCode, double grade, double credits) {
         this.moduleName = moduleName;
         this.moduleCode = moduleCode;
@@ -15,30 +22,66 @@ public class ModuleGrade {
         this.credits = credits;
     }
 
+    /**
+     * Method Gets Module Name
+     * @return returns a value
+     */
     public String getModuleName() {
         return moduleName;
     }
+
+    /**
+     * Method Gets Module Code
+     * @return returns a value
+     */
     public String getModuleCode(){
         return moduleCode;
     }
+
+    /**
+     * Method Gets Module Object and prints Module Information.
+     * @return returns a value
+     */
     public String getModule() {
         return String.format("%s - %s (%s)", moduleName, moduleCode, grade);
     }
 
-
+    /**
+     * Method Maps Grades and saves them as an array would
+     * @return returns a value
+     */
     public Map getGrade() {
         return grades;
     }
 
+    /**
+     * Method fetches credits
+     * @return returns a value
+     */
     public double getCredits() {
         return credits;
     }
+
+    /**
+     * Method Sets Module Name
+     * @param moduleName Module Name
+     */
     public void setModuleName(String moduleName){
         this.moduleName = moduleName;
     }
+
+    /**
+     * Method Sets Module Code
+     * @param moduleCode Code for specific Module 
+     */
     public void setModuleCode(String moduleCode){
         this.moduleCode = moduleCode;
     }
+
+    /**
+     * Returns to string for printing later on
+     * @return returns a value.
+     */
     @Override
     public String toString() {
         return moduleName + ": " + grades + " (" + credits + " credits)";
