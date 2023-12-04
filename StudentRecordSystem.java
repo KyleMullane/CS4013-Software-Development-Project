fa0721450a3b7aa1f56b1c804a0bd89b01ab2e00
KyleMullane <152344059+KyleMullane@users.noreply.github.com>
2023-12-04 17:37:45 +0000

    Update StudentRecordSystem.java

--- StudentRecordSystem.java
+++ StudentRecordSystem.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 
 /**
-  The Student Record System
+ * The Student Record System to Initialize Methods for "RunStudentRecordSystem"
+ * Initializes ArrayList to save Data: "Students", "Faculty Members", "Module Grades", "Module List"
  */
 public class StudentRecordSystem {
     private final ArrayList<Student> students;
@@ -17,18 +18,31 @@ public class StudentRecordSystem {
 
     /**
      * Method adds student to array list for menu.
-     * @param studentId
-     * @param name
-     * @param moduleCode
+     * @param studentId Students Unique ID.
+     * @param name Student's Name
+     * @param moduleCode Code of a specific Module
      */
     public void addStudent(String name, String studentId, String moduleCode, int yearOfStudy) {
         Student student = new Student(name, studentId,moduleCode,yearOfStudy);
         students.add(student);
     }
+
+    /**
+     * Method Adds Module to arrayList 
+     * @param moduleName Module Title.
+     * @param moduleCode Code of a specific Module.
+     * @param grade Grade.
+     * @param credits Credits Available in a Module
+     */
     public void addModule(String moduleName, String moduleCode,double grade, double credits){
         ModuleGrade module = new ModuleGrade(moduleName,moduleCode,grade,credits);
         modulesList.add(module);
     }
+
+    /**
+     * Method Fetches StudentTranscript and Displays
+     * @param studentId Students unique ID.
+     */
     public void getStudentTranscript(String studentId) {
         System.out.println("Transcript for Student ID: " + studentId);
         for (Student student : students) {
@@ -39,6 +53,10 @@ public class StudentRecordSystem {
         }
         System.out.println("Student not found with ID: " + studentId);
     }
+    /**
+     * getFacultyTranscript using Faculty ID to fetch a specific faculty members information and Displays.
+     * @param facultyId Faculty Members Unique ID.
+     */
     public void getFacultyTranscript(String facultyId){
         System.out.println("Transcript for Faculty Member - "+facultyId);
         for (Faculty faculty : facultyMembers){
@@ -48,6 +66,10 @@ public class StudentRecordSystem {
             }
         }
     }
+    /**
+     * getModule simply uses moduleCode to retrieve module information and print it.
+     * @param moduleCode Code of a specific Module
+     */
     public void getModule(String moduleCode){
         System.out.println("Module: "+ moduleCode);
         for (ModuleGrade module2 : modulesList){
@@ -57,8 +79,12 @@ public class StudentRecordSystem {
             }
         }
     }
+    /**
+     * getQCA uses Student ID to get QCA from saved csv file grades and uses QcaCalculator class to calculate QCA.
+     * @param studentId Students Unique ID.
+     */
     public void getQCA(String studentId){
         double qca = QcaCalculator.calculateQca("student_grades.csv");
-        System.out.println("/nQCA: " + qca);
+        System.out.println("/nQCA for Student: "+studentId+"/n"+qca);
     }
 }
