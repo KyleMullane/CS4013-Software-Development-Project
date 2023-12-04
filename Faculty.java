95b57dd544d13b1e2409064a6da036875801c154
KyleMullane <152344059+KyleMullane@users.noreply.github.com>
2023-12-04 18:35:23 +0000

    Update Faculty.java

--- Faculty.java
+++ Faculty.java
@@ -6,32 +6,52 @@ public class Faculty {
     private String facultyName;
     private List<String> modules;
 
+    /**
+     * Creates Object Faculty
+     * @param facultyId Faculty Member's ID
+     * @param facultyName Faculty Member's Name
+     */
     public Faculty(String facultyId, String facultyName) {
         this.facultyId = facultyId;
         this.facultyName = facultyName;
         this.modules = new ArrayList<>();
     }
 
+    /**
+     * Method Gets Faculty ID
+     * @return Returns a Value
+     */
     public String getFacultyId() {
         return facultyId;
     }
 
+    /**
+     * Method sets Faculty ID
+     * @param facultyId Faculty Member's ID
+     */
     public void setFacultyId(String facultyId) {
         this.facultyId = facultyId;
     }
 
+    /**
+     * Method gets Faculty Name 
+     * @return Returns a Value
+     */
     public String getFacultyName() {
         return facultyName;
     }
 
+    /**
+     * Method sets Faculty Member's Name
+     * @param facultyName Faculty Member's Name
+     */
     public void setFacultyName(String facultyName) {
         this.facultyName = facultyName;
     }
 
-    public void addModule(String moduleCode) {
-        modules.add(moduleCode);
-    }
-
+    /**
+     * Method Gets all Faculty Information and Prints Details.
+     */
     public void displayFacultyDetails() {
         System.out.println("Faculty ID: " + facultyId);
         System.out.println("Faculty Name: " + facultyName);
