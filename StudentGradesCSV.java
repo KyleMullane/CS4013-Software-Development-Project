dd617d7eed039f5e8fc992e749aa548c91bb8c72
KyleMullane <152344059+KyleMullane@users.noreply.github.com>
2023-12-04 21:56:25 +0000

    Update StudentGradesCSV.java

--- StudentGradesCSV.java
+++ StudentGradesCSV.java
@@ -1,30 +1,39 @@
 import java.io.*;
-public class StudentGradesCSV {
-    public static void main(String[] args) {
-        String file = "C:\\Users\\tommy\\OneDrive\\Documents\\student_Grade.csv"; //Provide the filepath
-        BufferedReader reader = null;
-        String line = ""; //create string to be used to reach each line of csv file
-        try {
-            reader = new BufferedReader(new FileReader(file));  //pass in file name(student_Grade_CSV.csv) to FileReader
-            while ((line = reader.readLine()) != null) {  //read next line of file and continue while loop, if there isnt a next line exit loop
+public class StudentGradesCSV {/**
+ * Method will Provide the filepath
+ * Create string to be used to reach each line of csv file
+ * pass in file name(student_Grade_CSV.csv) to FileReader
+ * read next line of file and continue while loop, if there isnt a next line exit loop
+ * split our line at all commas and assign values to a string array
+ * displaying strings with 12 spaces of room, left justified, display contents at index
+ * catch all exceptions
+ * Display errors if they occur
+ */
+public static void main(String[] args) {
+    String file = "C:\\Users\\tommy\\OneDrive\\Documents\\student_Grade.csv"; //Provide the filepath
+    BufferedReader reader = null;
+    String line = ""; //create string to be used to reach each line of csv file
+    try {
+        reader = new BufferedReader(new FileReader(file));  //pass in file name(student_Grade_CSV.csv) to FileReader
+        while ((line = reader.readLine()) != null) {  //read next line of file and continue while loop, if there isnt a next line exit loop
 
-                String[] row = line.split(",");  //split our line at all commas and assign values to a string array
-                for (String index : row) {
-                    System.out.printf("%-12s", index);   //displaying strings with 12 spaces of room, left justified, display contents at index
-                }
-                System.out.println();
+            String[] row = line.split(",");  //split our line at all commas and assign values to a string array
+            for (String index : row) {
+                System.out.printf("%-12s", index);   //displaying strings with 12 spaces of room, left justified, display contents at index
             }
-        } catch (Exception e) {  // catch all exceptions
-            e.printStackTrace();  // Display errors if they occur
-        } finally {
-            try {
-                if (reader != null) {
-                    reader.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
+            System.out.println();
         }
+    } catch (Exception e) {  // catch all exceptions
+        e.printStackTrace();  // Display errors if they occur
+    } finally {
+        try {
+            if (reader != null) {
+                reader.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
+}
 }
